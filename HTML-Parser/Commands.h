8c919ce0bc44061d6d128baa6fa95fe23943ef57
Tsvetelina Mancheva <72023208+manchevatsveti@users.noreply.github.com>
2024-11-04 14:41:51 +0200

    Add files via upload

---
+++ HTML-Parser/Commands.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "HTMLTable.h"
+
+#pragma once
+class Commands
+{
+	HTMLTable table;
+
+	void createAddFunc();
+	void createRemoveFunc();
+	void createEditFunc();
+	void createPrintFunc();
+	void createExitFunc(const char* filename);
+
+	bool validateIndex_addFunc(int id) const;
+	bool validateIndex_editFunc(int rowId, int colId) const;
+public:
+	void initCommand();
+
+};
+
---
+++ HTML-Parser/Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include "HTMLTable.h"
+#include "Commands.h"
+const int MAX_FILENAME = 100;
+
+int main() {
+
+	Commands c;
+	c.initCommand();
+}
